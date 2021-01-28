53b5fe4ff7fc53dfb5505139aa25ca76cde62298
Manish kumar <76118927+MANISH887@users.noreply.github.com>
2021-01-28 15:34:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-
 const url = (city) =>
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
@@ -11,7 +7,7 @@ async function getWeatherByLocation(city) {
     const resp = await fetch(url(city), { origin: "cors" });
     const respData = await resp.json();
 
-    console.log(respData);
+   // console.log(respData);
 
     addWeatherToPage(respData);
 }
